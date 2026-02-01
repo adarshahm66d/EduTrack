9ed24da3edce49aa7e8e0a522e81d5bd4f418b54
Chaitra <chaitra.mg@66degrees.com>
2026-02-01 13:58:59 +0530

    changed course cards to have thumbnails and video linkss

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -329,11 +329,9 @@ const AdminDashboard = () => {
                                         <div className="course-card-body">
                                             {course.link && (
                                                 <div className="course-link-info">
-                                                    <a 
-                                                        href={course.link} 
-                                                        target="_blank"
-                                                        rel="noopener noreferrer"
-                                                        className="playlist-link"
+                                                    <button 
+                                                        onClick={() => window.open(course.link, '_blank', 'noopener,noreferrer')}
+                                                        className="btn-playlist"
                                                     >
                                                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                                                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
@@ -341,7 +339,7 @@ const AdminDashboard = () => {
                                                             <line x1="10" y1="14" x2="21" y2="3"></line>
                                                         </svg>
                                                         View Playlist
-                                                    </a>
+                                                    </button>
                                                 </div>
                                             )}
                                         </div>
